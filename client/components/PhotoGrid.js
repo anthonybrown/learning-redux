ff8fcc7db90b0c7b32742c0da36d52bc4ab2d1aa
Tony Brown <tony@tonybrown.me>
2016-11-01 15:01:06 -0400

    setup React Router, converted components to ES6 classes

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PhotoGrid extends Component {
+  render() {
+    return (
+      <div className='photo-grid'>
+        <h3>I am the Photogrid Page</h3>
+      </div>
+    )
+  }
+}
+
+export default PhotoGrid
---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Single extends Component {
+  render() {
+    return (
+      <div className='single-photo'>
+        <h3>I am the single page</h3>
+      </div>
+    )
+  }
+}
+
+export default Single
